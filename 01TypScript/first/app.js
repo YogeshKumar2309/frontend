a2155211a649912ac1de00999fe3510960aea0a9
YogeshKumar2309 <placementsbyyogesh@gmail.com>
2025-11-23 14:34:46 +0530

    first ts program and number data types with youtube tutorial

---
+++ 01TypScript/first/app.js
@@ -0,0 +1,5 @@
+var user = "Yogesh kuamr";
+console.log(user);
+var num = 5;
+console.log(num);
+var username = 'yogesh';
---
+++ 01TypScript/first/app.ts
@@ -0,0 +1,6 @@
+var user: string = "Yogesh kuamr";
+console.log(user);
+var num : number= 5;
+console.log(num);
+var username : string = 'yogesh';
+
